c38f8309722f8a037fc6ba76a68f8e20357d1b26
gurumurthy974 <gurumurthysar974@gmail.com>
2024-07-16 10:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ terraform {
 
 
 
-module "terraform-multicloud-testscalr" {
-  source  = "multicloudcmritproton.scalr.io/acc-v0oef70o3ojc0ctf4/terraform-multicloud-testscalr/"
-  version = "1.0.1"
-  
+module "terraform-testscalr" {
+  source  = "/home/abhijith/terraform-testscalr"
+
+
 }
