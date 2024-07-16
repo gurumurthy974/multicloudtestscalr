92807b40abf35c171046a432ada74ed5aae3ec8a
gurumurthy974 <gurumurthysar974@gmail.com>
2024-07-16 10:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+
+terraform {
+  backend "remote" {
+    hostname = "multicloudcmritproton.scalr.io"
+    organization = "env-v0oef70o4bh06bqnu"
+
+    workspaces {
+      name = "MPC_Module"
+    }
+  }
+}
+
+
+
+
 module "terraform-multicloud-testscalr" {
   source  = "multicloudcmritproton.scalr.io/acc-v0oef70o3ojc0ctf4/terraform-multicloud-testscalr/"
   version = "1.0.1"
