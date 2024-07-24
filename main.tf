a574078f47de4696203e03d481f5505a9925679c
gurumurthy974 <gurumurthysar974@gmail.com>
2024-07-24 12:23:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
-terraform {
-  backend "remote" {
-    hostname = "multicloudcmritproton.scalr.io"
-    organization = "env-v0oef70o4bh06bqnu"
+//terraform {
+  //backend "remote" {
+    //hostname = "multicloudcmritproton.scalr.io"
+    //organization = "env-v0oef70o4bh06bqnu"
 
-    workspaces {
-      name = "MPC_Module"
-    }
-  }
-}
+    //workspaces {
+      //name = "MPC_Module"
+    //}
+  //}
+//}
 
